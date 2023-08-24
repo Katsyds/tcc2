eb3dd4d1449ae760d6f2e334654c056b5229717b
Renan <117865478+Katsyds@users.noreply.github.com>
2023-08-24 01:35:13 -0300

    1.5

---
+++ barbearia/js/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+// Conexão com o banco de dados MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'seu_usuario',
+  password: 'sua_senha',
+  database: 'barbershop'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão com o banco de dados MySQL estabelecida.');
+});
+
+// ...
+
+// Rota para agendar um horário
+app.post('/appointments', async (req, res) => {
+  try {
+    const { name, email, service, date, time } = req.body;
+
+    const sql = 'INSERT INTO appointments (name, email, service, date, time) VALUES (?, ?, ?, ?, ?)';
+    db.query(sql, [name, email, service, date, time], (err, result) => {
+      if (err) {
+        res.status(500).json({ error: 'Ocorreu um erro ao agendar o horário.' });
+        return;
+      }
+      res.status(201).json({ message: 'Horário agendado com sucesso!' });
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Ocorreu um erro ao agendar o horário.' });
+  }
+});
+
+// ...
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
