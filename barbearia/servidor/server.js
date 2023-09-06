fb16ee24546cc23a26febccf0fc86dcf8f781edc
Katsyds <oluveira.renan14@gmail.com>
2023-09-06 00:20:48 -0300

    beta 4

--- barbearia/servidor/server.js
+++ barbearia/servidor/server.js
@@ -19,48 +19,56 @@ db.run(`CREATE TABLE IF NOT EXISTS agendamentos (
   data TEXT
 )`);
 
-
-
-
 // Rota raiz, serve o arquivo index.html
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-// Rota para receber as informações do agendamento
 app.post('/agendamento', express.json(), (req, res) => {
-  const { nome, email, servico, data, horario } = req.body;
+  const { nome, horario, email, servico, data } = req.body;
 
-  db.run(`INSERT INTO agendamentos (nome, email, servico, horario, data) VALUES (?, ?, ?, ?, ?)`, [nome, email, servico, horario, data], (err) => {
-      if (err) {
-          console.error(err.message);
-          res.status(500).send('Erro ao agendar horário');
+  // Verificar se o horário já está ocupado
+  db.get(`SELECT COUNT(*) as count FROM agendamentos WHERE horario = ? AND DATE(horario) = DATE(?)`, [horario, horario], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Erro ao verificar horário');
+    } else {
+      const count = row.count;
+      if (count > 0) {
+        res.status(400).send('Horário já está ocupado para a data especificada');
       } else {
-          res.send('Horário agendado com sucesso');
+        // Inserir o novo agendamento com a data
+        db.run(`INSERT INTO agendamentos (nome, horario, email, servico, data) VALUES (?, ?, ?, ?, ?)`, [nome, horario, email, servico, data], (err) => {
+          if (err) {
+            console.error(err.message);
+            res.status(500).send('Erro ao agendar horário');
+          } else {
+            res.send('Horário agendado com sucesso');
+          }
+        });
       }
+    }
   });
 });
 
 
+// Rota para acessar a página de gerenciamento
+app.get('/gerenciamento', (req, res) => {
+  res.sendFile(__dirname + '/public/gerenciamento.html');
+});
 
-  // Rota para acessar a página de gerenciamento
-  app.get('/gerenciamento', (req, res) => {
-    res.sendFile(__dirname + '/public/gerenciamento.html');
+// Rota para obter os agendamentos do banco de dados
+app.get('/agendamentos', (req, res) => {
+  db.all(`SELECT * FROM agendamentos`, [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Erro ao buscar agendamentos');
+    } else {
+      res.json(rows);
+    }
   });
-  
-  
-  // Rota para obter os agendamentos do banco de dados
-  app.get('/agendamentos', (req, res) => {
-    db.all(`SELECT * FROM agendamentos`, [], (err, rows) => {
-      if (err) {
-        console.error(err.message);
-        res.status(500).send('Erro ao buscar agendamentos');
-      } else {
-        res.json(rows);
-      }
-    });
-  });
-  
+});
+
 // Inicia o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
