51b44af56bbb3e9b057df48b8de22c957f2b9295
Katsyds <oluveira.renan14@gmail.com>
2023-09-25 22:42:17 -0300

    Funcional 1

--- barbearia/servidor/server.js
+++ barbearia/servidor/server.js
@@ -3,14 +3,11 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 4002;
 
-// Configuração do middleware para servir arquivos estáticos da pasta "public"
 app.use(express.static('public'));
-app.use(express.json()); // Parse JSON no corpo das solicitações
+app.use(express.json());
 
-// Conectar ao banco de dados SQLite
 const db = new sqlite3.Database('db.sqlite');
 
-// Criar a tabela de agendamentos se ela não existir
 db.run(`CREATE TABLE IF NOT EXISTS agendamentos (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   nome TEXT,
@@ -20,12 +17,10 @@ db.run(`CREATE TABLE IF NOT EXISTS agendamentos (
   data TEXT
 )`);
 
-// Rota raiz, serve o arquivo index.html
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-// Rota para obter os agendamentos do banco de dados
 app.get('/agendamentos', (req, res) => {
   db.all(`SELECT * FROM agendamentos`, [], (err, rows) => {
     if (err) {
@@ -37,11 +32,9 @@ app.get('/agendamentos', (req, res) => {
   });
 });
 
-// Rota para agendar um horário
 app.post('/agendamento', express.json(), (req, res) => {
   const { nome, horario, email, servico, data } = req.body;
 
-  // Verificar se o horário já está ocupado para a data especificada
   db.get(`SELECT COUNT(*) as count FROM agendamentos WHERE horario = ? AND data = ?`, [horario, data], (err, row) => {
     if (err) {
       console.error(err.message);
@@ -51,7 +44,6 @@ app.post('/agendamento', express.json(), (req, res) => {
       if (count > 0) {
         res.status(400).send('Horário já está ocupado para esta data.');
       } else {
-        // Inserir o novo agendamento com data e hora
         db.run(`INSERT INTO agendamentos (nome, horario, email, servico, data) VALUES (?, ?, ?, ?, ?)`, [nome, horario, email, servico, data], (err) => {
           if (err) {
             console.error(err.message);
@@ -65,13 +57,10 @@ app.post('/agendamento', express.json(), (req, res) => {
   });
 });
 
-// Rota para acessar a página de gerenciamento
 app.get('/gerenciamento', (req, res) => {
   res.sendFile(__dirname + '/public/gerenciamento.html');
 });
 
-
-// Inicia o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
